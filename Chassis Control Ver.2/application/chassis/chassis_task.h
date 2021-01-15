17686203d37447398f746eaecb3994ac4a084c59
Buwei-He <70881012+Buwei-He@users.noreply.github.com>
2021-01-15 08:57:14 +0800

    Add files via upload

---
+++ Chassis Control Ver.2/application/chassis/chassis_task.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+ *  Copyright (C) 2020 RoboMaster.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
+#ifndef __CHASSIS_TASK_H__
+#define __CHASSIS_TASK_H__
+
+#include "chassis.h"
+#include "can.h"
+#include "drv_can.h"
+
+#define CHASSIS_CAN &hcan2
+#define GIMBAL_CAN &hcan1
+
+typedef enum
+{
+    CAN_CHASSIS_ALL_ID = 0x200,
+    CAN_3508_M1_ID = 0x201,
+    CAN_3508_M2_ID = 0x202,
+    CAN_3508_M3_ID = 0x203,
+    CAN_3508_M4_ID = 0x204,
+
+    CAN_YAW_MOTOR_ID = 0x205,
+    CAN_PIT_MOTOR_ID = 0x206,
+    CAN_TRIGGER_MOTOR_ID = 0x207,
+    CAN_GIMBAL_ALL_ID = 0x1FF,
+
+} can_msg_id_e;
+
+struct chassis *get_chassis(void);
+void chassis_task(void const *argument);
+int32_t chassis_set_relative_angle(float angle);
+int32_t follow_angle_info_rcv(uint8_t *buff, uint16_t len);
+void set_follow_relative(float val);
+
+#endif // __CHASSIS_TASK_H__
